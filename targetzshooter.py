2022d36ef7f27a94af3f6df4137d4fcd5fcd8d26
Andriw <andriw30davi@gmail.com>
2025-02-28 18:10:31 -0300

    Classe do Alvo

--- targetzshooter.py
+++ targetzshooter.py
@@ -27,9 +27,20 @@ class Button:
         if self.rect.collidepoint(mouse_pos):
             pygame.draw.rect(screen, (0, 200, 0), self.rect, 5)
         screen.blit(self.image, self.rect)
-# Classe do Alvo
 
+# Classe do Alvo
 class Target(pygame.sprite.Sprite):
+    def __init__(self, x, y, is_bomb=False):
+        super().__init__()
+        self.is_bomb = is_bomb
+        if self.is_bomb:
+            self.image = pygame.image.load('bomba.png')
+        else:
+            self.image = pygame.image.load('alvo.png')
+        
+        self.image = pygame.transform.scale(self.image, (90, 90))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
 
 # Classe do Jogo
 class Game:
