fe0c921f7ab3f17061709e48e496fc867134b6e2
Andriw <andriw30davi@gmail.com>
2025-02-28 15:21:29 -0300

    Estrutura primordial do código

---
+++ targetzshooter.py
@@ -0,0 +1,43 @@
+import pygame
+import random
+import sys
+
+pygame.init()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+font = pygame.font.SysFont('Pixelify Sans Regular', 35)
+font_game_over = pygame.font.SysFont('Pixelify Sans Regular', 60)
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 768
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Targetz Shooter")
+
+# Classe do Botão
+class Button:
+
+# Classe do Alvo
+class Target(pygame.sprite.Sprite):
+
+# Classe do Jogo
+class Game:
+
+# Função que exibe a tela de Game Over
+def game_over():
+
+
+# Função que exibe a tela de créditos
+def credits_screen():
+
+
+# Função que exibe a tela de dificuldade
+def difficulty_screen():
+
+
+# Função que exibe o menu
+def main_menu():
+
+
+def main():
